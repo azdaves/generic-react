21dbf63963337d0700dd1d98bee1d6fa83cfacfd
Dave Schilling <ds@Daves-MacBook-Pro.local>
2018-11-09 14:36:23 -0700

    add files for a prototype app counter

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,10 +1,53 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class Counter extends Component {
-    state = {  }
-    render() { 
-        return (  );
-    }
+  state = {
+    count: 0,
+    imageUrl: "https://picsum.photos/200",
+    tags: ["tag1", "tag2", "tag3", "tag4", "tag5"]
+  };
+
+  //   constructor() {
+  //     super();
+  //     this.handleIncrement = this.handleIncrement.bind(this);
+  //   }
+
+  // add the increment handler for the viewed button. arrow function used to bind this
+  //this.setState() notifies react that state is changed
+  handleIncrement = () => {
+    this.setState({ count: this.state.count + 1 });
+  };
+
+  render() {
+    return (
+      <div>
+        <img src={this.state.imageUrl} alt="" />
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+        <button
+          onClick={this.handleIncrement}
+          className="btn btn-secondary btn-sm"
+        >
+          Viewed
+        </button>
+        <ul>
+          {this.state.tags.map(tag => (
+            <li key={tag}>{tag}</li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+    return classes;
+  }
+
+  formatCount() {
+    const { count } = this.state;
+    return count === 0 ? "Zero" : count;
+  }
 }
- 
+
 export default Counter;
